e3722012fa266aaacb88f75e3e2c96ea73f59b6e
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-06-13 17:34:02 -0500

    Finished home page and routing to each specific post by Id

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,48 +1,50 @@
 import axios from "axios";
-import { useState } from "react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
-export async function getServerSideProps(context) {
+export default function Home() {
+  const [users, setUsers] = useState([]);
 
-    const { data } = await axios.get("https://jsonplaceholder.typicode.com/users")
+  async function fetchUsers() {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    setUsers(data);
+  }
 
-    return {
-        props: {
-            initialUsers: data,
-        }
-    }
-}
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
-
-
-export default function Home({ initialUsers }) {
-  const [users, setUsers] = useState(initialUsers)
-
-  console.log(users)
   return (
     <>
-      <div className="container">
-        <div className="row">
-          <div className="user-list">
-            <div className="user">
-              <div className="user-card">
-                <div className="user-card__container">
-                  <h3>user.name</h3>
-                  <p>
-                    <b>Email:</b> user.email
-                  </p>
-                  <p>
-                    <b>Phone:</b> user.phone
-                  </p>
-                  <p>
-                    <b>Website:</b>
-                    user.website
-                  </p>
+      {users?.map((user) => (
+        <Link href={`/posts/${user.id}`} key={user.id}>
+          <div className="container" >
+            <div className="row">
+              <div className="user-list">
+                <div className="user">
+                  <div className="user-card">
+                    <div className="user-card__container">
+                      <h3>{user.name}</h3>
+                      <p>
+                        <b>Email:</b> {user.email}
+                      </p>
+                      <p>
+                        <b>Phone:</b> {user.phone}
+                      </p>
+                      <p>
+                        <b>Website:</b>
+                        {user.website}
+                      </p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
+        </Link>
+      ))}
     </>
   );
 }
