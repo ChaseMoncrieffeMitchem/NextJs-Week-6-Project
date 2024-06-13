3798729cae96ac03fcda1b90198ef530d5a423b8
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-06-13 01:47:28 -0500

    Added server side rendering when fetching the API

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,7 +1,48 @@
-export default function Home() {
+import axios from "axios";
+import { useState } from "react";
+
+export async function getServerSideProps(context) {
+
+    const { data } = await axios.get("https://jsonplaceholder.typicode.com/users")
+
+    return {
+        props: {
+            initialUsers: data,
+        }
+    }
+}
+
+
+
+export default function Home({ initialUsers }) {
+  const [users, setUsers] = useState(initialUsers)
+
+  console.log(users)
   return (
     <>
-        Home 
+      <div className="container">
+        <div className="row">
+          <div className="user-list">
+            <div className="user">
+              <div className="user-card">
+                <div className="user-card__container">
+                  <h3>user.name</h3>
+                  <p>
+                    <b>Email:</b> user.email
+                  </p>
+                  <p>
+                    <b>Phone:</b> user.phone
+                  </p>
+                  <p>
+                    <b>Website:</b>
+                    user.website
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -1,58 +1,76 @@
 import axios from "axios";
-import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
-export default function PostsById() {
-  const params = useParams();
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState();
-  const [search, setSearch] = useState(params?.id)
+export async function getServerSideProps(context) {
+  const { id } = context.params;
 
-  async function getPostsById() {
+  const { data } = await axios.get(
+    `https://jsonplaceholder.typicode.com/posts?userId=${id}`
+  );
+
+  return {
+    props: {
+      initialPosts: data,
+    },
+  };
+}
+
+export default function PostsById({ initialPosts }) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [searchInput, setSearchInput] = useState("");
+  const [posts, setPosts] = useState(initialPosts);
+
+  const fetchPosts = async (userId) => {
     setLoading(true);
     const { data } = await axios.get(
-      `https://jsonplaceholder.typicode.com/posts?userId=${params.id || search}`
+      `https://jsonplaceholder.typicode.com/posts?userId=${userId}`
     );
     setPosts(data);
     setLoading(false);
-    setSearch(params.id)
-    console.log(data);
-  }
+  };
 
-  useEffect(() => {
-    if (params !== null) {
-      getPostsById();
-    }
-  }, [params?.id]);
+  const onSearch = () => {
+    fetchPosts(searchInput);
+  };
 
   return (
     <>
       <div>
         <div className="post__search">
-          <button>← Back</button>
+          <Link href="/">
+            <button>← Back</button>
+          </Link>
           <div className="post__search--container">
             <label className="post__search--label">Search by Id</label>
-            <input type="number" value={search} onChange={(event) => setSearch(event.target.value)}/>
-            <button onClick={() => setSearch(params?.id)}>Enter</button>
+            <input
+              type="number"
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+              onKeyPress={(e) => e.key === "Enter" && onSearch()}
+            />
+            <button onClick={onSearch}>Enter</button>
           </div>
         </div>
-        {loading ? (
-          <div className="post">
-            <div className="post__title">
-              <div className="post__title--skeleton"></div>
-            </div>
-            <div className="post__body">
-              <p className="post__body--skeleton"></p>
-            </div>
-          </div>
-        ) : (
-          posts.map((post) => (
-            <div className="post" key={post.id}>
-              <div className="post__title">{post.title}</div>
-              <p className="post__body">{post.body}</p>
-            </div>
-          ))
-        )}
+        {loading
+          ? new Array(10).fill(0).map((_, index) => (
+              <div className="post" key={index}>
+                <div className="post__title">
+                  <div className="post__title--skeleton"></div>
+                </div>
+                <div className="post__body">
+                  <p className="post__body--skeleton"></p>
+                </div>
+              </div>
+            ))
+          : posts?.map((post) => (
+              <div className="post" key={post.id}>
+                <div className="post__title">{post.title}</div>
+                <p className="post__body">{post.body}</p>
+              </div>
+            ))}
       </div>
     </>
   );
